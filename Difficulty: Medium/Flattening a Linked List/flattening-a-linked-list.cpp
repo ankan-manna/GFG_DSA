6d9f90a7249db473491e51586ee537cc21b928a2
Ankan Manna <149871785+ankan-manna@users.noreply.github.com>
2025-02-14 10:36:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.cpp
@@ -0,0 +1,125 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+    struct Node* bottom;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+        bottom = NULL;
+    }
+};
+
+void printList(Node* head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->bottom;
+    }
+    printf("\n");
+}
+
+Node* createLinkedList(vector<Node*>& v) {
+    Node* head = new Node(0);
+    Node* temp = head;
+    int n = v.size();
+    for (int i = 0; i < n; i++) {
+        temp->next = v[i];
+        temp = temp->next;
+    }
+    return head->next;
+}
+
+
+// } Driver Code Ends
+/* Node structure  used in the program
+
+struct Node{
+    int data;
+    struct Node * next;
+    struct Node * bottom;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+        bottom = NULL;
+    }
+
+};
+*/
+
+class Solution {
+  public:
+    // Function which returns the  root of the flattened linked list.
+    Node *flatten(Node *root) {
+        // Your code here
+        if(!root) return NULL;
+        vector<int>v;
+       struct Node *n=root,*b=NULL;
+       while(n){
+           v.push_back(n->data);
+           if(n->bottom){
+               b=n->bottom;
+               while(b){
+                   v.push_back(b->data);
+                   b=b->bottom;
+               }
+           }
+           n=n->next;
+       }
+       
+       sort(v.begin(),v.end());
+       struct Node* list=new Node(0);
+       struct Node* ptr=list;
+      for(auto x:v){
+          ptr->bottom=new Node(x);
+          ptr=ptr->bottom;
+      }
+      return list->bottom;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int n;
+        cin >> n;
+        cin.ignore();
+
+        vector<Node*> v(n);
+
+        for (int i = 0; i < n; i++) {
+            string line;
+            getline(cin, line);
+            stringstream ss(line);
+
+            Node* head = new Node(0);
+            Node* temp = head;
+            int x;
+            while (ss >> x) {
+                Node* newNode = new Node(x);
+                temp->bottom = newNode;
+                temp = temp->bottom;
+            }
+            v[i] = head->bottom;
+        }
+
+        Solution ob;
+        Node* list = createLinkedList(v);
+        Node* head = ob.flatten(list);
+        printList(head);
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
